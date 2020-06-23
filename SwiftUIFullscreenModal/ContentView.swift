0a7d93af3a98370352a6d44046e30e953a0b8ac6
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 08:33:51 +0300

    init

--- SwiftUIFullscreenModal/ContentView.swift
+++ SwiftUIFullscreenModal/ContentView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isFullScreen = false
+    @State private var isNotFullScreen = false
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        HStack {
+            Text("Not full screen")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .onTapGesture {
+                    isNotFullScreen.toggle()
+                }
+                .sheet(isPresented: $isNotFullScreen) {
+                    NotFullScreen(isNotFullSceen: $isNotFullScreen)
+                }
+            
+            Text("Full screen")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .onTapGesture {
+                    isFullScreen.toggle()
+                }
+                .fullScreenCover(isPresented: $isFullScreen) {
+                    FullScreen(isFullSceen: $isFullScreen)
+                }
+        }
+    }
+}
+
+struct NotFullScreen: View {
+    @Binding var isNotFullSceen: Bool
+    
+    var body: some View {
+        Text("This is not full screen.")
+            .onTapGesture {
+                self.isNotFullSceen.toggle()
+            }
+    }
+}
+
+struct FullScreen: View {
+    @Binding var isFullSceen: Bool
+    
+    var body: some View {
+        Text("This is full screen.")
+            .onTapGesture {
+                self.isFullSceen.toggle()
+            }
     }
 }
 
