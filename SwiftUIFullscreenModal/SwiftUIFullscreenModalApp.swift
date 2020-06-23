0d1d8b6918d5084641cdd007d23da1a3a67e0577
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 08:18:44 +0300

    Initial Commit

---
+++ SwiftUIFullscreenModal/SwiftUIFullscreenModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFullscreenModalApp.swift
+//  SwiftUIFullscreenModal
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFullscreenModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
